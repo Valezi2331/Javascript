8d63cbb9bf45d5a923007ac87fa414594f51e147
Leonardo Valezi <leo.valezi@outlook.com>
2025-04-03 21:26:40 -0300

    Fiz uma caixa de texto que coleta um hobbie do usuário e imprime na tela

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function Saudacao() { // função do botão de saudação
     let texto = "Como vai você?"; 
     document.getElementById("mensagem").textContent = texto;
 } 
+
+function guardarHobbie(){
+    let inputHobbie = document.getElementById("hobbie")
+    let hobbie = inputHobbie.value
+    document.getElementById("respostaHobbie").textContent = hobbie;
+}
