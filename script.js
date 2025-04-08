1bf2be3d99011c730bcfee0675f5ac94968e9178
Leonardo Valezi <leo.valezi@outlook.com>
2025-04-07 21:46:55 -0300

    Botões adicionados com sucesso

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
-let hobbies = []; // Array global para armazenar hobbies
+let hobbies = []
 
 function Saudacao() {
-    let texto = "Como vai você?";
+    let texto = "Como vai você?"
     document.getElementById("mensagem").textContent = texto;
 }
 
 function guardarHobbie() {
-    let inputHobbie = document.getElementById("hobbie");
-    let hobbie = inputHobbie.value.trim();
-    let resposta = document.getElementById("respostaHobbie");
+    let inputHobbie = document.getElementById("hobbie")
+    let hobbie = inputHobbie.value.trim()
+    let resposta = document.getElementById("respostaHobbie")
 
     if (hobbie == "") {
-        let mensagemErro = "Por favor, preencha o campo de hobbie!";
-        resposta.textContent = mensagemErro;
-        alert(mensagemErro);
+        let mensagemErro = "Por favor, preencha o campo de hobbie!"
+        resposta.textContent = mensagemErro
+        alert(mensagemErro)
     } else {
         resposta.textContent = "Hobbie adicionado à lista";
-        hobbies.push(hobbie); // Adiciona ao array GLOBAL
-        MostrarHobbie();
+        hobbies.push(hobbie);
+        MostrarHobbie()
     }
-    inputHobbie.value = "";
+    inputHobbie.value = ""
 }
 
 function MostrarHobbie() {
     let lista = document.getElementById("listaHobbies");
-    lista.innerHTML = ""; // Limpa a lista antes de recriar
+    lista.innerHTML = ""
 
     for (let i = 0; i < hobbies.length; i++) {
-        let novoHobbie = document.createElement("li");
-        novoHobbie.textContent = hobbies[i]; // Pega o hobbie correto
-        lista.appendChild(novoHobbie);
+        let novoHobbie = document.createElement("li")
+        novoHobbie.textContent = hobbies[i]
+
+        let botaoRemover = document.createElement("button")
+        botaoRemover.className = "remove"
+        botaoRemover.textContent = "Remover"
+        botaoRemover.onclick = () => RemoverHobbie(i)
+
+        let botaoEditar = document.createElement("button")
+        botaoEditar.className = "editar"
+        botaoEditar.textContent = "Editar"
+        botaoEditar.onclick = () => EditaHobbie(i)
+
+        novoHobbie.appendChild(botaoRemover)
+        novoHobbie.appendChild(botaoEditar)
+        lista.appendChild(novoHobbie)
     }
+
+function RemoverHobbie(i) {
+    hobbies.splice(i, 1)
+    MostrarHobbie()
+}
+
+function EditaHobbie(i) {
+    let hobbieEditado = prompt("Edite o hobbie:")
+    if (hobbieEditado.trim !== ""){
+        hobbies[i] = hobbieEditado
+        MostrarHobbie()
+    }
+}
+
+}
+
+function Limpar(){
+    hobbies.length = 0
+    MostrarHobbie()
+    const resposta = document.getElementById("respostaHobbie")
+    resposta.textContent = "Lista de hobbies limpa!"
 }
